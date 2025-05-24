3e869ce0d5cbb43ab039aafc1d757550d219b7d0
Shirochi-stack <noanimenolife03@gmail.com>
2025-05-24 04:45:41 +0400

    Update launch_Glossarion.bat

--- src/launch_Glossarion.bat
+++ src/launch_Glossarion.bat
@@ -1,9 +1,9 @@
 @echo off
 cd /d "%~dp0"
 
-where pythonw >nul 2>&1
+where python >nul 2>&1
 if %ERRORLEVEL% neq 0 (
-    echo ❌ pythonw.exe not found. Please install Python and ensure it is in your PATH.
+    echo ❌ python.exe not found. Please install Python and ensure it is in your PATH.
     pause
     exit /b
 )
